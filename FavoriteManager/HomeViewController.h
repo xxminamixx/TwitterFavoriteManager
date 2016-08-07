69bf34dd2f20aa7f259145ba41bb6a360e30abe8
minami <minami@iMac.local>
2016-08-07 10:29:30 +0900

    起動時に表示されるViewのControllerを作成

---
+++ FavoriteManager/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  FavoriteManager
+//
+//  Created by 南　京兵 on 2016/08/07.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ FavoriteManager/HomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.m
+//  FavoriteManager
+//
+//  Created by 南　京兵 on 2016/08/07.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
